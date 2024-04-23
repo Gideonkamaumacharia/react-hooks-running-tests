9518a70b977d901f07a9296ebbf71420a5685bc6
Gideon Macharia <kamaugedion95@gmail.com>
2024-04-23 17:57:19 +0300

    modify file

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,7 +1,13 @@
+import "@testing-library/jest-dom";
+import { render, screen } from "@testing-library/react";
 import React from "react";
 
-function Article(props) {
-  return<p>please pass this test</p>
-  
-}
-export default Article;
+// import the component you wrote
+import Article from "../components/Article";
+
+// test the component
+test("displays the text 'please pass this test'", () => {
+  render(<Article />);
+
+  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
+});
