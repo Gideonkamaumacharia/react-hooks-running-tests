3436c141df50a270e231309a4275e00b87714358
Gideon Macharia <kamaugedion95@gmail.com>
2024-04-23 08:52:41 +0300

    initial commit

--- src/__tests__/Article.test.js
+++ src/__tests__/Article.test.js
@@ -1,11 +1,7 @@
-import "@testing-library/jest-dom";
-import { render, screen } from "@testing-library/react";
 import React from "react";
 
-import Article from "../components/Article";
-
-test("displays the text 'please pass this test'", () => {
-  render(<Article />);
-
-  expect(screen.queryByText("please pass this test")).toBeInTheDocument();
-});
+function Article(props) {
+  return<p>please pass this test</p>
+  
+}
+export default Article;
